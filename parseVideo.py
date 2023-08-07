c3811d04fb14be234a3fbe17e3b37ad2a508d190
Edgar Hernandez <edgarhernandez00@gmail.com>
2023-08-06 20:21:15 -0700

    [Major Update] Version: 2.0
    - Updates library to support images and videos
    - now uses MIME to identify video, audio or images
    - will sort to folders by media orientation
    - Can now tag and update file metadata tag with orientation tag.

--- parseVideo.py
+++ parseVideo.py
@@ -1,47 +1,65 @@
-
-import os
-import sys
 import subprocess
 import datetime
-import argparse
 import json
+import click
+from pathlib import Path
+import mimetypes
+import shutil
+from PIL import Image
 
 
-# ffprobe = os.path.join(os.path.dirname(sys.argv[0])  + '/ffprobe')
-ffprobe = "/usr/local/bin/ffprobe"
-def getVideoStreamDuration (videoObject):
-    foundDuration = None
-    streamDuration = None
-    for i in range(len(videoObject)):
-        
-        if videoObject['streams'][i]["codec_type"] == "video":
-            streamDuration = str(videoObject['streams'][i]["duration"])
+
+ffprobe_path = shutil.which('ffprobe')
+if ffprobe_path is None:
+    raise Exception('ffprobe not found on the system')
+
+
+def get_video_stream_duration(video_object):
+    found_duration = None
+    stream_duration = None
+    for i in range(len(video_object)):
+
+        if video_object['streams'][i]["codec_type"] == "video":
+            stream_duration = str(video_object['streams'][i]["duration"])
             break
-        elif videoObject['streams'][i]["codec_type"] == "audio":
-            streamDuration = str(videoObject['streams'][i]["duration"])
+        elif video_object['streams'][i]["codec_type"] == "audio":
+            stream_duration = str(video_object['streams'][i]["duration"])
             break
-    formatedDuration =  datetime.datetime.strftime(datetime.datetime.strptime(streamDuration.strip(), "%H:%M:%S.%f"), "%H:%M:%S:%f")
-    return formatedDuration
+    formated_duration = datetime.datetime.strftime(datetime.datetime.strptime(stream_duration.strip(), "%H:%M:%S.%f"), "%H:%M:%S:%f")
+    return formated_duration
 
-def getVideoAspectRatio (videoObject):
-    for i in range(len(videoObject)):
-        
-        if videoObject['streams'][i]["codec_type"] == "video":
-            videoWidth = int(videoObject['streams'][i]["width"])
-            videoHeight = int(videoObject['streams'][i]["height"])
-            videoAspectRatio = videoWidth/videoHeight
+
+def get_video_aspect_ratio(video_object):
+    for i in range(len(video_object)):
+
+        if video_object['streams'][i]["codec_type"] == "video":
+            video_width = int(video_object['streams'][i]["width"])
+            video_height = int(video_object['streams'][i]["height"])
+            video_aspect_ratio = video_width / video_height
             break
-    return videoAspectRatio
+    return video_aspect_ratio
 
-def sortByAspect (file ,videoAspectRatio):
-    if videoAspectRatio == 0.5625:
-        print( "Portrait")
+
+def sort_by_aspect(file, video_aspect_ratio):
+    if video_aspect_ratio == 0.5625:
+        print("Portrait")
+        metadata = 'orientation=portrait'
     else:
         print("Landscape")
+        metadata = 'orientation=landscape'
+    file_path = Path(file)
+    output_file = Path('tagged_videos') / file_path.name
+    try:
+        subprocess.run(['ffmpeg', '-i', str(file_path),  '-update', '-metadata', metadata, '-codec', 'copy', '-y', str(output_file)], check=True)
+        shutil.move(str(file_path), Path('processed_videos') / file_path.name)
+    except subprocess.CalledProcessError as e:
+        print(f"Error tagging {file_path}: {e}")
+    except shutil.Error as e:
+        print(f"Error moving {file_path}: {e}")
 
-def sortByDuration(file,videoDuration):
-    (h, m, s, ms) = videoDuration.split(':')
-    print (h, m, s, ms)
+def sort_by_duration(file, video_duration):
+    (h, m, s, ms) = video_duration.split(':')
+    print(h, m, s, ms)
     if int(s) < 10:
         print(s)
         print("Less than 10 seconds")
@@ -49,42 +67,59 @@ def sortByDuration(file,videoDuration):
         print("Longer than 10 seconds")
 
 
-def parseVideoData(videoFileList):
-    for file in videoFileList:
-
-        stdout, stderr=subprocess.Popen([ffprobe, "-sexagesimal", "-print_format", "json", "-show_entries", "stream=codec_type,codec_name,duration,sample_rate,bit_rate,width,height", file, "-sexagesimal"] , universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate() 
-        videoMetaData = json.loads(stdout)
-        videoDuration = getVideoStreamDuration(videoMetaData)
-        videoAspectRatio = getVideoAspectRatio(videoMetaData)
-        sortByDuration(file,videoDuration)
-        sortByAspect(file,videoAspectRatio)
-        
+def parse_video_data(video_file_list):
+    for file in video_file_list:
+        stdout, _ = subprocess.Popen([ffprobe_path, "-sexagesimal", "-print_format", "json", "-show_entries", "stream=codec_type,codec_name,duration,sample_rate,bit_rate,width,height", file, "-sexagesimal"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        video_meta_data = json.loads(stdout)
+        video_duration = get_video_stream_duration(video_meta_data)
+        video_aspect_ratio = get_video_aspect_ratio(video_meta_data)
+        sort_by_duration(file, video_duration)
+        sort_by_aspect(file, video_aspect_ratio)
 
 
 
 
+def get_image_aspect_ratio(image_file):
+    try:
+        with Image.open(image_file) as img:
+            width, height = img.size
+            return width / height
+    except (IOError, ValueError):
+        return None
 
-acceptedFormats = ('.avi', '.mp4', '.mp3', '.mxf', '.mov', '.wav', '.aif')
+
+def sort_images_by_aspect_ratio(image_files):
+    for image_file in image_files:
+        aspect_ratio = get_image_aspect_ratio(image_file)
+        if aspect_ratio is not None:
+            sort_by_aspect(image_file, aspect_ratio)
+
+
+@click.command()
+@click.option('-v', '--videos', type=click.Path(exists=True), help='Path to video files')
+@click.option('-i', '--images', type=click.Path(exists=True), help='Path to image files')
+def main(videos, images):
+    if videos:
+            video_files = []
+            for path in Path(videos).rglob('*'):
+                if path.is_file() and mimetypes.guess_type(str(path))[0].startswith('video/'):
+                    video_files.append(str(path))
+            if not video_files:
+                print('No accepted video files found.')
+            else:
+                parse_video_data(video_files)
+
+    if images:
+        image_files = []
+        for path in Path(images).rglob('*'):
+            mime_type = mimetypes.guess_type(str(path))[0]
+            if mime_type is not None and mime_type.startswith('image/'):
+                image_files.append(str(path))
+        if not image_files:
+            print('No accepted image files found.')
+        else:
+            sort_images_by_aspect_ratio(image_files)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A program that generates metadata summaries and can extract audio from video files")
-    parser.add_argument("-f", "--files", nargs="*", help="Indivudal files or directories to process")
-
-    args = parser.parse_args()
-
-    fileList = []
-    for files in args.files:
-        if os.path.isdir(files):
-            directoryFiles = sorted(os.listdir(files))
-            for file in directoryFiles:
-                if file.endswith(acceptedFormats):
-                    fileList.append(os.path.join(files, file))
-        elif os.path.isfile(files):
-            if files.endswith(acceptedFormats):
-                fileList.append(os.path.abspath(files))
-
-    sourceFiles = sorted(fileList)
-    if not sourceFiles:
-        print('No accepted files found. Drag files or folders or both.')
-    else:
-        videoAttributes = parseVideoData(sourceFiles)
+    main()
